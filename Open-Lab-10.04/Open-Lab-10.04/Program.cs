20eb908642229d5e196380b5b93317a9f60c4cbf
Lukáš Lokša <lukasloksa2@gmail.com>
2021-12-07 15:04:37 +0100

    Add constructors into previous exercise

--- Open-Lab-10.04/Open-Lab-10.04/Program.cs
+++ Open-Lab-10.04/Open-Lab-10.04/Program.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 
-namespace Open_Lab_10._03
+namespace Open_Lab_10._04
 {
     class Program
     {
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            //LOTR.title = "Title";    
-            //Console.WriteLine(LOTR.title);
-            LOTR.Title("Title");
+            Book LOTR2 = new Book();
+            Book LOTR3 = new Book("Titulok", 50);
+            Book HOBIT = new Book("Titulok", 50, "Kategoria", "Autor", 2020);
+
+            LOTR.Title("Titulok");
             LOTR.Pages(50);
-            LOTR.Category("Category");
+            LOTR.Category("Kategoria");
             LOTR.Author("Author");
             LOTR.ReleaseDate(2020);
             Console.WriteLine(LOTR.ToString());
+            Console.WriteLine(LOTR2.ToString());
+            Console.WriteLine(LOTR3.ToString());
+            Console.WriteLine(HOBIT.ToString());
         }
     }
-    
-    
+
+
     public class Book
     {
         public string title;
@@ -27,34 +32,62 @@ namespace Open_Lab_10._03
         public string author;
         public int releaseDate;
 
-        public void Title(string NewTitle) 
-        { 
+
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+        public Book(string title, int pages)
+        {
+            this.title = title;
+            this.pages = pages;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+        public Book(string title, int pages, string category, string author, int releaseDate)
+        {
+            this.title = title;
+            this.pages = pages;
+            this.category = category;
+            this.author = author;
+            this.releaseDate = releaseDate;
+        }
+
+
+        public void Title(string NewTitle)
+        {
             title = NewTitle;
         }
-        public void Pages(int NewPages) 
+        public void Pages(int NewPages)
         {
-            pages = NewPages; 
+            pages = NewPages;
             if (pages < 0)
             {
                 pages = 1;
             }
         }
-        public void Category(string NewCategory) 
-        { 
+        public void Category(string NewCategory)
+        {
             category = NewCategory;
         }
-        public void Author(string NewAuthor) 
-        { 
+        public void Author(string NewAuthor)
+        {
             author = NewAuthor;
         }
-        public void ReleaseDate(int NewReleaseDate) 
-        { 
+        public void ReleaseDate(int NewReleaseDate)
+        {
             releaseDate = NewReleaseDate;
             if (releaseDate < 1450 || releaseDate > 2021)
             {
                 releaseDate = -1;
             }
         }
+
 
 
         public override string ToString()
